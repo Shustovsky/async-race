5bf48d0d265fcea9d9d708b08360848db0fa78f5
A. Shustovsky <a.shustovsky@gmail.com>
2023-01-18 09:50:16 +0400

    init: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.js$/, loader: "source-map-loader"
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'public'),
+        assetModuleFilename: 'assets/[ext]',
+    },
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new EslingPlugin({extensions: 'ts'}),
+    ]
+};
+
+module.exports = ({mode}) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};

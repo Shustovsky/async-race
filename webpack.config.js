f6d239d27abdc5b376e7de58a7c0d1b7c188de35
A. Shustovsky <a.shustovsky@gmail.com>
2023-01-18 10:16:51 +0400

    fix: fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index'),
@@ -22,12 +23,7 @@ const baseConfig = {
             },
             {
                 test: /\.(scss|css)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
             },
             {
                 test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
@@ -38,7 +34,7 @@ const baseConfig = {
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'public'),
-        assetModuleFilename: 'assets/[ext]',
+        assetModuleFilename: path.join('[name][ext]'),
     },
     resolve: {
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
